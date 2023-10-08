b1417798cfa337a046365bc42070e93d34753f1d
gary <keijiro.im@gmail.com>
2023-10-09 00:33:48 +0900

    promptコマンドの追加

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,38 @@
 #!/usr/bin/env node
 
 import * as oac from "./oac";
+import * as Fs from "fs";
 
 function main() {
+    const env = new oac.OacEnv();
     const option = new oac.OacOption();
     const messages:string[] = [];
 
     for (let i = 2; i < process.argv.length; i++) {
         let arg = process.argv[i];
-        if(arg === "chat") {
-            option.command = oac.OacCommand.Chat;
-        }
-        else if(arg === "clear") {
-            option.command = oac.OacCommand.Clear;
-        }
-        else if(arg === "save") {
-            option.command = oac.OacCommand.Save;
+        if(arg === oac.OacCommand.Chat ||
+            arg === oac.OacCommand.Save ||
+            arg === oac.OacCommand.Clear ||
+            arg === oac.OacCommand.Prompt ) {
+            option.command = arg;
         }
         else if(arg === "--debug") {
             option.debug = true;
         }
         else if(arg.startsWith("--model=")) {
             option.model = arg.replace(/^--model=/, "");
+        }
+        else if(arg.startsWith("--result=")) {
+            option.resultPath = arg.replace(/^--result=/, "");
+        }
+        else if(arg.startsWith("--input=")) {
+            option.input = arg.replace(/^--input=/, "");
+        }
+        else if(arg.startsWith("--output=")) {
+            option.output = arg.replace(/^--output=/, "");
+        }
+        else if(arg.startsWith("--temperature=")) {
+            option.temperature = parseFloat(arg.replace(/^--temperature=/, ""));
         }
         else if(arg === "--silent") {
             option.silent = true;
@@ -42,6 +53,18 @@ function main() {
                 else {
                     console.log(`不明なパラメータが指定されています: ${arg}`);
                     return;
+                }
+            }
+            else if(option.command === oac.OacCommand.Prompt) {
+                if(!option.prompt) {
+                    option.prompt = env.rootDir + "/prompt/" + arg + ".yaml"
+                    if(!Fs.existsSync(option.prompt)) {
+                        console.log(`promptが存在しません: ${arg}`);
+                        return;
+                    }
+                }
+                else {
+                    option.params.push(arg);
                 }
             }
             else {
@@ -65,31 +88,47 @@ function main() {
         console.log("usage: oac [options] [command] [parameters]");
         console.log("  options");
         console.log("      --silent");
+        console.log("      --mdeo=[model]");
         console.log("  command");
+        console.log("      [message]");
         console.log("      chat [message]");
         console.log("      save [filename]");
+        console.log("      pm   [prompt] [parameter]...");
         console.log("      clear");
         return;
     }
 
-    const client = new oac.OacClient();
-    if(option.command === oac.OacCommand.Chat) {
-        if(option.message) {
-            client.chat(option);
+    try {
+        const client = new oac.OacClient();
+        if(option.command === oac.OacCommand.Chat) {
+            if(option.message) {
+                client.chat(option);
+            }
+            else {
+                throw new Error("メッセージを入力してください");
+            }      
+        }
+        else if(option.command === oac.OacCommand.Clear) {
+            client.clear(option);
+        }
+        else if(option.command === oac.OacCommand.Save) {
+            if(option.savePath) {
+                client.save(option);
+            }
+            else {
+                throw new Error("ファイル名を指定してください");
+            }
+        }
+        else if(option.command === oac.OacCommand.Prompt) {
+            client.prompt(option);
+        }
+    }
+    catch(e) {
+        if(e instanceof Error) {
+            console.log(e.message);
         }
         else {
-            console.log("メッセージを入力してください");
-        }      
-    }
-    else if(option.command === oac.OacCommand.Clear) {
-        client.clear(option);
-    }
-    else if(option.command === oac.OacCommand.Save) {
-        if(option.savePath) {
-            client.save(option);
-        }
-        else {
-            console.log("ファイル名を指定してください");
+            console.log(e);
         }
     }
 }
--- src/oac/index.ts
+++ src/oac/index.ts
@@ -1,3 +1,4 @@
 export * from './OacOption';
+export * from './OacEnv';
 export * from './OacClient';
 
